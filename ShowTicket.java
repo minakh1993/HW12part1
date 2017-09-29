b5ad22708ec38c057339beb949a8fd2b598eb17e
minakh1993 <minakhoshnevisan1993@gmail.com>
2017-09-29 13:21:34 +0330

    ticket update servlet added successfully

--- ShowTicket.java
+++ ShowTicket.java
@@ -17,23 +17,25 @@ import database.TicketEntryManager;
 @WebServlet("/ShowTicket")
 public class ShowTicket extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ShowTicket() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ShowTicket() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		TicketEntryManager entryManager=new TicketEntryManager();
-		
+		TicketEntryManager entryManager = new TicketEntryManager();
+
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
@@ -41,67 +43,66 @@ public class ShowTicket extends HttpServlet {
 		out.println("<title>Ticket information</title>");
 		out.println("</head>");
 		out.println("<body>");
-		
-		int code=0;
-		try{
-		code=Integer.parseInt(request.getParameter("TicketCode"));
-		System.out.println(code);
-		}catch(Exception e){
+
+		int code = 0;
+		try {
+			code = Integer.parseInt(request.getParameter("TicketCode"));
+			System.out.println(code);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Ticket ticket=entryManager.showTicketByNumber(code);
-		if(ticket!=null){
-			//means the code was right 
+		Ticket ticket = entryManager.showTicketByNumber(code);
+		if (ticket != null) {
+			// means the code was right
 			out.println("<br>");
-			out.println("<b>Ticket information with code '"+code+"' :</b>");
+			out.println("<b>Ticket information with code '" + code + "' :</b>");
 			out.print("<b>");
 			out.println("<br>");
-			out.print("customer name:");out.println(ticket.getCustomerName());
+			out.print("customer name:");
+			out.println(ticket.getCustomerName());
 			out.println("<br>");
-			out.print("origin:");out.println(ticket.getOrigin());
+			out.print("origin:");
+			out.println(ticket.getOrigin());
 			out.println("<br>");
-			out.print("destination:");out.println(ticket.getDestination());
+			out.print("destination:");
+			out.println(ticket.getDestination());
 			out.println("<br>");
-			out.print("flight number:");out.println(ticket.getFlightNumber());
+			out.print("flight number:");
+			out.println(ticket.getFlightNumber());
 			out.println("<br>");
-			out.print("date:");out.println(ticket.getDate());
+			out.print("date:");
+			out.println(ticket.getDate());
 			out.println("<br>");
-			
+
 			out.print("</b>");
-			
-			
-			
-			
-			
-		}else{
-			//means the code is wrong
+
+		} else {
+			// means the code is wrong
 			out.println("<b>");
 			out.println("wrong Ticket code enter again");
-			out.print("enterd code was:");out.print(code);
+			out.print("enterd code was:");
+			out.print(code);
 			out.println("</b>");
-			
+
 		}
-		
+
 		out.println("<br>");
-    	out.println("<br>");
-    	out.println("<a href=\"Ticket.html\">");
+		out.println("<br>");
+		out.println("<a href=\"Ticket.html\">");
 		out.println("<b>go to first page</b>");
 		out.println("</a>");
-		
 
 		out.println("</body>");
 		out.println("</html>");
-		
-		
-		
-		
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
